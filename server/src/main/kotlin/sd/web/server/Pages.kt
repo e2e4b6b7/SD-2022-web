940855ecb95c9291eb719ece2835c414ed935d51
DF5HSE <fild10@yandex.ru>
2022-05-21 20:12:33 +0300

    Add html pages generators impl

--- server/src/main/kotlin/sd/web/server/Pages.kt
+++ server/src/main/kotlin/sd/web/server/Pages.kt
@@ -1,14 +1,13 @@
 package sd.web.server
 
 import kotlinx.html.*
-import sd.web.server.data.Homework
-import sd.web.server.data.Submission
+import sd.web.server.data.*
 
-enum class Role {
-    STUDENT, TEACHER
+enum class Role(val root: String) {
+    STUDENT("/student"), TEACHER("teacher")
 }
 
-fun HTML.homeworksListPage(homeworks: List<Homework>, role: Role) {
+fun HTML.homeworksListPage(homeworks: List<HomeworkWithId>, role: Role) {
     head {
         title {
             +"Homeworks"
@@ -16,33 +15,92 @@ fun HTML.homeworksListPage(homeworks: List<Homework>, role: Role) {
     }
     body {
         if (role == Role.TEACHER) {
-            form {
-                // TODO form for creating homework and call rout like 'post teacher/new_homework'
+            text("New homework")
+            form(action = "${role.root}/homework", method = FormMethod.post) {
+                name = "New homework"
+                input(type = InputType.text, name = "title") {
+                    required = true
+                    placeholder = "Title"
+                }
+
+                input(type = InputType.text, name = "description") {
+                    placeholder = "Description"
+                }
+                text("Publication date and time:")
+                input(type = InputType.dateTimeLocal, name = "publicationDate") {
+                    required = true
+                }
+                text("Deadline:")
+                input(type = InputType.dateTimeLocal, name = "deadline")
+                input(type = InputType.submit)
             }
         }
         table {
-            // TODO table of homeworks with possibility for click and opening homework page
+            tr {
+                th { +"ID" }
+                th { +"Title" }
+                th { +"Publication time" }
+                th { +"Deadline" }
+            }
+            homeworks.forEach {
+                tr {
+                    td {
+                        a(href = "${role.root}/homework/${it.id}") {
+                            +it.id.toString()
+                        }
+                    }
+                    td { +it.title }
+                    td { +it.publicationTime.toString() }
+                    td { +it.deadline.toString() }
+                }
+            }
         }
     }
 }
 
-fun HTML.homeworkPage(homework: Homework, role: Role) {
+fun HTML.homeworkPage(homework: HomeworkWithId, role: Role) {
     head {
         title {
             +"Homework: ${homework.title}"
         }
     }
     body {
-        // TODO display info about task
+        table {
+            tr {
+                th { +"Homework ID" }
+                td { +homework.id.toString() }
+            }
+            tr {
+                th { +"Title" }
+                td { +homework.title }
+            }
+            tr {
+                th { +"Description" }
+                td { +(homework.description ?: "No description") }
+            }
+            tr {
+                th { +"Deadline" }
+                td { +(homework.deadline?.toString() ?: "No deadline") }
+            }
+            tr {
+                th { +"Publication time" }
+                td { +homework.publicationTime.toString() }
+            }
+        }
         if (role == Role.STUDENT) {
-            form {
-                // TODO form for submit and call rout like 'post student/new_submit'
+            text("New submission")
+            form(action = "${role.root}/submission", method = FormMethod.post) {
+                input(type = InputType.url, name = "solution") {
+                    required = true
+                    placeholder = "URL to github with solution"
+                }
+                input(type = InputType.submit)
             }
         }
     }
 }
 
-fun HTML.submissionsListPage(submissions: List<Submission>) {
+fun HTML.submissionsListPage(submissions: List<SubmissionWithId>, role: Role) {
     head {
         title {
             +"Submissions"
@@ -50,19 +108,65 @@ fun HTML.submissionsListPage(submissions: List<Submission>) {
     }
     body {
         table {
-            // TODO table of submissions
+            tr {
+                th { +"Submission time" }
+                th { +"Submission ID" }
+                th { +"Homework ID" }
+                th { +"Checking result" }
+            }
+            submissions.forEach {
+                tr {
+                    td { +it.time.toString() }
+                    td {
+                        a(href = "${role.root}/submission/${it.id}") { +it.id.toString() }
+                    }
+                    td {
+                        a(href = "${role.root}/homework/${it.homeworkId}") { +it.homeworkId.toString() }
+                    }
+                    td {
+                        +"TODO"
+                    }
+                }
+            }
         }
     }
 }
 
-fun HTML.submissionPage(submission: Submission) {
+fun HTML.submissionPage(submission: SubmissionWithId, role: Role) {
     head {
         title {
             +"Submission for homework ${submission.homeworkId}"
         }
     }
     body {
-        // TODO display info about submission
+        table {
+            tr {
+                th { +"Submission ID" }
+                td { submission.id.toString() }
+            }
+            tr {
+                th { +"Submission time" }
+                td { +submission.time.toString() }
+            }
+            tr {
+                th { +"Homework" }
+                td {
+                    a(href = "${role.root}/homework/${submission.homeworkId}") {
+                        +submission.homeworkId.toString()
+                    }
+                }
+            }
+            tr {
+                th { +"Solution" }
+                td {
+                    a(href = submission.solution) { +submission.solution }
+                }
+            }
+            tr {
+                th { +"Checking result" }
+                td { +"TODO" }
+            }
+        }
     }
 }
 
